ed3eadc7f643e0be05166abf2b51b063a30ea9c6
Vasko Kelkocev <pisimina@gmail.com>
2015-06-30 11:03:32 +0200

    Upadated to be currency aware

--- ExpenditureHistory/sqlcontent.sql
+++ ExpenditureHistory/sqlcontent.sql
@@ -1,74 +1,80 @@
-select CATEGNAME,MonthAgo0,MonthAgo1,MonthAgo2,MonthAgo3,MonthAgo4,MonthAgo5,MonthAgo6,MonthAgo7,TOTAL,
-
-	strftime('%m-%Y', date('now')) as date0,
-	strftime('%m-%Y', date('now', 'start of month', '-1 month')) as date1,
-	strftime('%m-%Y', date('now', 'start of month', '-2 month')) as date2,
-	strftime('%m-%Y', date('now', 'start of month', '-3 month')) as date3,
-	strftime('%m-%Y', date('now', 'start of month', '-4 month')) as date4,
-	strftime('%m-%Y', date('now', 'start of month', '-5 month')) as date5,
-	strftime('%m-%Y', date('now', 'start of month', '-6 month')) as date6,
-	strftime('%m-%Y', date('now', 'start of month', '-7 month')) as date7
-
+select CATEGNAME,MonthAgo0,MonthAgo1,MonthAgo2,MonthAgo3,MonthAgo4,MonthAgo5,MonthAgo6,MonthAgo7,MonthAgo8,TOTAL
 	from CATEGORY_V1 as c
 	left join (
-		select t.CATEGID, sum(t.TRANSAMOUNT) as Total
+		select t.CATEGID, sum(t.TRANSAMOUNT * cf.BaseConvRate) as Total
 		from CHECKINGACCOUNT_V1 as t
-		where t.TRANSCODE='Withdrawal' and t.TRANSDATE >= date('now', 'start of month', '-7 month') and t.TRANSDATE < date('now', 'start of month', '-0 month')
+		left join ACCOUNTLIST_V1 AC on AC.ACCOUNTID = t.ACCOUNTID
+		left join currencyformats_v1 cf on cf.currencyid=AC.currencyid
+		where t.TRANSCODE='Withdrawal' and t.TRANSDATE >= date('now', 'start of month', '-8 month')
 		group by t.CATEGID
 		order by Total desc ) as total
 	on c.CATEGID=total.CATEGID
 
 	left join (
-		select t.CATEGID, sum(t.TRANSAMOUNT) as MonthAgo0
+		select t.CATEGID, sum(t.TRANSAMOUNT * cf.BaseConvRate) as MonthAgo0
 		from CHECKINGACCOUNT_V1 as t
+		left join ACCOUNTLIST_V1 AC on AC.ACCOUNTID = t.ACCOUNTID
+		left join currencyformats_v1 cf on cf.currencyid=AC.currencyid
 		where t.TRANSCODE='Withdrawal' and t.TRANSDATE >= date('now', 'start of month', '-0 month')
 		group by t.CATEGID
 		order by MonthAgo0 desc ) as m0
 	on c.CATEGID=m0.CATEGID
-
+	
 	left join (
-		select t.CATEGID, sum(t.TRANSAMOUNT) as MonthAgo1
+		select t.CATEGID, sum(t.TRANSAMOUNT * cf.BaseConvRate) as MonthAgo1
 		from CHECKINGACCOUNT_V1 as t
+		left join ACCOUNTLIST_V1 AC on AC.ACCOUNTID = t.ACCOUNTID
+		left join currencyformats_v1 cf on cf.currencyid=AC.currencyid
 		where t.TRANSCODE='Withdrawal' and t.TRANSDATE >= date('now', 'start of month', '-1 month') and t.TRANSDATE < date('now', 'start of month', '-0 month')
 		group by t.CATEGID
 		order by MonthAgo1 desc ) as m1
 	on c.CATEGID=m1.CATEGID
 
 	left join (
-		select t.CATEGID, sum(t.TRANSAMOUNT) as MonthAgo2
+		select t.CATEGID, sum(t.TRANSAMOUNT * cf.BaseConvRate) as MonthAgo2
 		from CHECKINGACCOUNT_V1 as t
+		left join ACCOUNTLIST_V1 AC on AC.ACCOUNTID = t.ACCOUNTID
+		left join currencyformats_v1 cf on cf.currencyid=AC.currencyid
 		where t.TRANSCODE='Withdrawal' and t.TRANSDATE >= date('now', 'start of month', '-2 month') and t.TRANSDATE < date('now', 'start of month', '-1 month')
 		group by t.CATEGID
 		order by MonthAgo2 desc ) as m2
 	on c.CATEGID=m2.CATEGID
 
 	left join (
-		select t.CATEGID, sum(t.TRANSAMOUNT) as MonthAgo3
+		select t.CATEGID, sum(t.TRANSAMOUNT * cf.BaseConvRate) as MonthAgo3
 		from CHECKINGACCOUNT_V1 as t
+		left join ACCOUNTLIST_V1 AC on AC.ACCOUNTID = t.ACCOUNTID
+		left join currencyformats_v1 cf on cf.currencyid=AC.currencyid
 		where t.TRANSCODE='Withdrawal' and t.TRANSDATE >= date('now', 'start of month', '-3 month') and t.TRANSDATE < date('now', 'start of month', '-2 month')
 		group by t.CATEGID
 		order by MonthAgo3 desc ) as m3
 	on c.CATEGID=m3.CATEGID
 
 	left join (
-		select t.CATEGID, sum(t.TRANSAMOUNT) as MonthAgo4
+		select t.CATEGID, sum(t.TRANSAMOUNT * cf.BaseConvRate) as MonthAgo4
 		from CHECKINGACCOUNT_V1 as t
+		left join ACCOUNTLIST_V1 AC on AC.ACCOUNTID = t.ACCOUNTID
+		left join currencyformats_v1 cf on cf.currencyid=AC.currencyid
 		where t.TRANSCODE='Withdrawal' and t.TRANSDATE >= date('now', 'start of month', '-4 month') and t.TRANSDATE < date('now', 'start of month', '-3 month')
 		group by t.CATEGID
 		order by MonthAgo4 desc ) as m4
 	on c.CATEGID=m4.CATEGID
 
 	left join (
-		select t.CATEGID, sum(t.TRANSAMOUNT) as MonthAgo5
+		select t.CATEGID, sum(t.TRANSAMOUNT * cf.BaseConvRate) as MonthAgo5
 		from CHECKINGACCOUNT_V1 as t
+		left join ACCOUNTLIST_V1 AC on AC.ACCOUNTID = t.ACCOUNTID
+		left join currencyformats_v1 cf on cf.currencyid=AC.currencyid
 		where t.TRANSCODE='Withdrawal' and t.TRANSDATE >= date('now', 'start of month', '-5 month') and t.TRANSDATE < date('now', 'start of month', '-4 month')
 		group by t.CATEGID
 		order by MonthAgo5 desc ) as m5
 	on c.CATEGID=m5.CATEGID
 
 	left join (
-		select t.CATEGID, sum(t.TRANSAMOUNT) as MonthAgo6
+		select t.CATEGID, sum(t.TRANSAMOUNT * cf.BaseConvRate) as MonthAgo6
 		from CHECKINGACCOUNT_V1 as t
+		left join ACCOUNTLIST_V1 AC on AC.ACCOUNTID = t.ACCOUNTID
+		left join currencyformats_v1 cf on cf.currencyid=AC.currencyid
 		where t.TRANSCODE='Withdrawal' and t.TRANSDATE >= date('now', 'start of month', '-6 month') and t.TRANSDATE < date('now', 'start of month', '-5 month')
 		group by t.CATEGID
 		order by MonthAgo6 desc ) as m6
@@ -76,13 +82,26 @@ select CATEGNAME,MonthAgo0,MonthAgo1,MonthAgo2,MonthAgo3,MonthAgo4,MonthAgo5,Mon
 
 
 	left join (
-		select t.CATEGID, sum(t.TRANSAMOUNT) as MonthAgo7
+		select t.CATEGID, sum(t.TRANSAMOUNT * cf.BaseConvRate) as MonthAgo7
 		from CHECKINGACCOUNT_V1 as t
+		left join ACCOUNTLIST_V1 AC on AC.ACCOUNTID = t.ACCOUNTID
+		left join currencyformats_v1 cf on cf.currencyid=AC.currencyid
 		where t.TRANSCODE='Withdrawal' and t.TRANSDATE >= date('now', 'start of month', '-7 month') and t.TRANSDATE < date('now', 'start of month', '-6 month')
 		group by t.CATEGID
 		order by MonthAgo7 desc ) as m7
 	on c.CATEGID=m7.CATEGID
 
 
+	left join (
+		select t.CATEGID, sum(t.TRANSAMOUNT * cf.BaseConvRate) as MonthAgo8
+		from CHECKINGACCOUNT_V1 as t
+		left join ACCOUNTLIST_V1 AC on AC.ACCOUNTID = t.ACCOUNTID
+		left join currencyformats_v1 cf on cf.currencyid=AC.currencyid
+		where t.TRANSCODE='Withdrawal' and t.TRANSDATE >= date('now', 'start of month', '-8 month') and t.TRANSDATE < date('now', 'start of month', '-7 month')
+		group by t.CATEGID
+		order by MonthAgo8 desc ) as m8
+	on c.CATEGID=m8.CATEGID
+
 	where total>0
-	order by TOTAL desc limit 30;
+	order by TOTAL desc limit 15
+	;
