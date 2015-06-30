533d36dc942d1ad8f483a076c6a059eea05eb75f
vkelk <pisimina@gmail.com>
2015-06-30 10:26:35 +0200

    Upadated to be currency aware
    added BaseConvRate factor

--- ExpenseAndRevenueByYear/sqlcontent.sql
+++ ExpenseAndRevenueByYear/sqlcontent.sql
@@ -7,19 +7,21 @@ select
     (SELECT sum(initialbal) FROM accountlist_V1) as initialbal
 from (  
   select 
-    strftime('%Y', TRANSDATE) as periode,
+    strftime('%Y', c.TRANSDATE) as periode,
     case
-      when transcode = 'Deposit' then transamount
+      when c.transcode = 'Deposit' then c.transamount * cf.BaseConvRate
       else 0
     end as Deposit,
     case
-          when transcode = 'Withdrawal' then -transamount
+          when c.transcode = 'Withdrawal' then -c.transamount * cf.BaseConvRate
           else 0
     end as Withdrawal
     --,*
   from
-    checkingaccount_V1
-  where status <>'V'
+    checkingaccount_V1 c
+	left join ACCOUNTLIST_V1 AC on AC.ACCOUNTID = c.ACCOUNTID
+	left join currencyformats_v1 cf on cf.currencyid=AC.currencyid
+  where c.status <>'V'
 )
 group by periode
 order by periode asc
